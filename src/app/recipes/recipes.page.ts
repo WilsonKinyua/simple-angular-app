b8be79538500c62b0adf0dd161498f62c5406a6a
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-01-17 22:49:14 +0300

    generate pages according to recipes id

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
+  recipes: Recipe[] = [
+    { 
+      id: 'r1',
+      title: 'Pancakes',
+      imageUrl: 'https://images.pexels.com/photos/574111/pexels-photo-574111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      ingredients: ['1 ½ cups all-purpose flour', '3 ½ teaspoons baking powder','1 teaspoon salt']
+    },
+    { 
+      id: 'r2',
+      title: 'Pizza',
+      imageUrl: 'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      ingredients: ['1 ½ cups all-purpose Pizza', '3 ½ teaspoons baking pizza','1 teaspoon pizza']
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
